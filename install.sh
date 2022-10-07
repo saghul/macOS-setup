db0ef183fa9fdcdba1986fb865430cddee7e2564
Saúl Ibarra Corretgé <s@saghul.net>
2022-10-07 10:02:07 +0200

    Add installer script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+TARGET=~/.macOS-setup
+
+rm -rf ${TARGET}
+mkdir ${TARGET}
+
+cd ${TARGET}
+curl -s -L -o - https://github.com/saghul/macOS-setup/archive/refs/heads/master.tar.gz | tar zx --strip-components 1
+
+exec ./run
